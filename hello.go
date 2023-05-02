782b83851c8aa7b6b7e3501283c830853816225a
Joseph Boyce <joseph.boyce@bjss.com>
2023-05-02 15:36:57 +0100

    code

--- hello.go
+++ hello.go
@@ -4,26 +4,71 @@ import (
 	"fmt"
 ) 
 
-func main() {
-	a:=1.0
-	b:=7.0
-	c:=9.0
-	total:= a+b+c
-	fmt.Println("Total=",total)
 
-	item1:=1.95
-	item2:=4.95
-	item3:=7.99
-	totalprice:= item1+item2+item3
-	fmt.Println("Total price=",totalprice)
+func main(){
 
-	ttotal:= totalprice+total
-	fmt.Println("Total before Vat=",ttotal)
+	count:=10
+	name:=1
+	for name <=count {
+		fmt.Println("Joseph")
 
-	totalvat:=ttotal *20 /100 
-	fmt.Println("Total with vat=",totalvat+ttotal)
+		name++
 
-	fmt.Println("VAT 20%=",totalvat)
+	}
+
+
+	total:=1
+	for sum:= 1; sum <= 10; sum++ {
+		total*=sum
+	}
+    fmt.Println(total)
+}
+
+	
+	
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// func main() {
+	//a:=1.0
+	//b:=7.0
+	//c:=9.0
+	//total:= a+b+c
+	//fmt.Println("Total=",total)
+
+	//item1:=1.95
+	//item2:=4.95
+	//item3:=7.99
+	//totalprice:= item1+item2+item3
+	//.fmt.Println("Total price=",totalprice)
+
+	//ttotal:= totalprice+total
+	//fmt.Println("Total before Vat=",ttotal)
+
+	//totalvat:=ttotal *20 /100 
+	//fmt.Println("Total with vat=",totalvat+ttotal)
+
+	//fmt.Println("VAT 20%=",totalvat)
      
 
-}
+ // }
+
+
