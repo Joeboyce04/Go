8cdf098ae67d54e5bf943108f105d0135b50d3e1
Joseph Boyce <joseph.boyce@bjss.com>
2023-05-02 12:27:10 +0100

    vat

--- hello.go
+++ hello.go
@@ -5,6 +5,25 @@ import (
 ) 
 
 func main() {
-	var firstname = "Joseph"
-	fmt.Println("Hello",firstname)
+	a:=1.0
+	b:=7.0
+	c:=9.0
+	total:= a+b+c
+	fmt.Println("Total",total)
+
+	item1:=1.95
+	item2:=4.95
+	item3:=7.99
+	totalprice:= item1+item2+item3
+	fmt.Println("Total price",totalprice)
+
+	ttotal:= totalprice+total
+	fmt.Println("Total before Vat",ttotal)
+
+	totalvat:=ttotal / 100 * 20
+	fmt.Println("Total with vat",totalvat+ttotal)
+
+	fmt.Println("VAT 20%",totalvat)
+     
+
 }
