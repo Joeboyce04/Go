b8041b0ee3b1255eead27004ea79d7e9774bde46
Joseph Boyce <joseph.boyce@bjss.com>
2023-05-02 12:43:56 +0100

    update

--- hello.go
+++ hello.go
@@ -9,21 +9,21 @@ func main() {
 	b:=7.0
 	c:=9.0
 	total:= a+b+c
-	fmt.Println("Total",total)
+	fmt.Println("Total=",total)
 
 	item1:=1.95
 	item2:=4.95
 	item3:=7.99
 	totalprice:= item1+item2+item3
-	fmt.Println("Total price",totalprice)
+	fmt.Println("Total price=",totalprice)
 
 	ttotal:= totalprice+total
-	fmt.Println("Total before Vat",ttotal)
+	fmt.Println("Total before Vat=",ttotal)
 
-	totalvat:=ttotal / 100 * 20
-	fmt.Println("Total with vat",totalvat+ttotal)
+	totalvat:=ttotal *20 /100 
+	fmt.Println("Total with vat=",totalvat+ttotal)
 
-	fmt.Println("VAT 20%",totalvat)
+	fmt.Println("VAT 20%=",totalvat)
      
 
 }
